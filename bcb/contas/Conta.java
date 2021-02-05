a254a25070292d6871ff0dde0f0ae06885462518
Charlie Fonseca <charlierf@gmail.com>
2021-02-04 21:13:25 -0300

    Criação do esqueleto de Conta e ContaCorrente

--- bcb/contas/Conta.java
+++ bcb/contas/Conta.java
@@ -2,4 +2,114 @@ package bcb.contas;
 
 public abstract class Conta {
     
+    private String codigo;
+    
+    private Banco banco;
+    
+    private Agencia agencia;
+    
+    private Cliente cliente;
+    
+    protected int saldoConta;
+    
+    private String [] chavePix = new String[5];
+
+    public Conta(){}
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Banco getBanco() {
+        return banco;
+    }
+
+    public void setBanco(Banco banco) {
+        this.banco = banco;
+    }
+
+    public Agencia getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(Agencia agencia) {
+        this.agencia = agencia;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public int getSaldoConta() {
+        return saldoConta;
+    }
+
+    public void setSaldoConta(int saldoConta) {
+        this.saldoConta = saldoConta;
+    }
+
+    public String[] getChavePix() {
+        return chavePix;
+    }
+
+    public void setChavePix(String[] chavePix) {
+        this.chavePix = chavePix;
+    }
+
+    public void depositarValor(int x){
+        this.saldoConta += x;
+    }
+
+    public abstract void sacarValor(int x);
+
+    public abstract void obterExtrato();
+
+    public abstract void pix(String chave, int valor);
+
+    public void gerarPixEmail(){
+        int j = 0;
+        for (int i = 0; !this.chavePix[i].isEmpty() && i <= chavePix.length; i++) {
+            j = i;
+        }
+        if (j != 6){   
+        this.chavePix[j] = this.cliente.getEmail();
+        }
+        }
+    }
+
+    public void gerarPixTelefone(){
+        int j = 0;
+        for (int i = 0; !this.chavePix[i].isEmpty() && i < chavePix.length; i++) {
+            j = i;
+        }
+        if (j != 6){   
+            this.chavePix[j] = this.cliente.getTelefone();
+            }
+        }
+
+        public Conta(String codigo, Banco banco, Agencia agencia, Cliente cliente, int saldoConta, String[] chavePix) {
+            this.codigo = codigo;
+            this.banco = banco;
+            this.agencia = agencia;
+            this.cliente = cliente;
+            this.saldoConta = saldoConta;
+            this.chavePix = chavePix;
+        }
+    }
+
+
+
+    
+    
+
+    
+
 }
--- bcb/contas/ContaCorrente.java
+++ bcb/contas/ContaCorrente.java
@@ -1,5 +1,64 @@
 package bcb.contas;
 
 public class ContaCorrente extends Conta {
+
+    private int taxaManutencao;
+
+    private int chequeEspecial;
+
+    public ContaCorrente(){}    
+
+
+    public int getTaxaManutencao() {
+        return taxaManutencao;
+    }
+
+    public void setTaxaManutencao(int taxaManutencao) {
+        this.taxaManutencao = taxaManutencao;
+    }
+
+    public int public int getChequeEspecial() {
+        return chequeEspecial;
+    }
+
+    public void setChequeEspecial(int chequeEspecial) {
+        this.chequeEspecial = chequeEspecial;
+    }
+    
+    @Override
+    public void sacarValor(int x) {
+         if (super.saldoConta > x){
+            super.saldoConta -= x;
+         } else {
+             if ((super.saldoConta + this.chequeEspecial) > x){
+                 this.chequeEspecial -= (x - super.saldoConta);
+                 super.saldoConta = 0;
+             } else {
+                 System.out.println("Saldo insuficiente e não há limite no cheque especial!");
+             }
+         }
+    }
+    
+    @Override
+    public void obterExtrato() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void pix(String chave, int valor) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public ContaCorrente() {
+    }
+
+    public ContaCorrente(String codigo, Banco banco, Agencia agencia, Cliente cliente, int saldoConta,
+            String[] chavePix, int taxaManutencao, int chequeEspecial) {
+        super(codigo, banco, agencia, cliente, saldoConta, chavePix);
+        this.taxaManutencao = taxaManutencao;
+        this.chequeEspecial = chequeEspecial;
+    }
     
 }
